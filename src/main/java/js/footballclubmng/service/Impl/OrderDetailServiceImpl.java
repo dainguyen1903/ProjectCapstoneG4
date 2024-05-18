9479dd7c61a8deb7241ce4671f98fc33042ceb71
dainguyen1903 <dainguyencva@gmail.com>
2024-05-18 17:41:36 +0700

    Change name file OrderDetailService

--- src/main/java/js/footballclubmng/service/Impl/OrderDetailServiceImpl.java
+++ src/main/java/js/footballclubmng/service/Impl/OrderDetailServiceImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class OrderDetailImpl implements OrderDetailService {
+public class OrderDetailServiceImpl implements OrderDetailService {
     @Autowired
     OrderRepository orderRepository;
 
