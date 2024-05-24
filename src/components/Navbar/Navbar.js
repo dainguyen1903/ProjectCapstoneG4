ffee8bf995b51dcabb4fd8e4a4c06829690b06b8
ducbv10 <ducbv1005@gmail.com>
2024-05-24 10:27:40 +0700

    hide sidebar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -52,13 +52,7 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="navbar-cnt flex align-center">
         <div className="brand-and-toggler flex align-center">
-          <button
-            type="button"
-            className="sidebar-show-btn text-white"
-            onClick={() => dispatch(setSidebarOn())}
-          >
-            <i className="fas fa-bars"></i>
-          </button>
+        
           <Link to="/" className="navbar-brand flex align-center">
             <span className="navbar-brand-ico">
               <i className="fa-solid fa-bag-shopping"></i>
